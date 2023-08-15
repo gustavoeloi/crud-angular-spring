95773928e9c6daa91c6ffee1b8f629123a3ffe52
Gustavo Henrique Eloi Domingues <08323517177@mma.gov.br>
2023-08-15 14:06:05 -0300

    adicionando os arquivos do frontend

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss'],
+})
+export class CoursesComponent {
+  courses$: Observable<Course[]>;
+  displayedColumns: string[] = ['name', 'category'];
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = coursesService.listFindAll().pipe(
+      catchError((error) => {
+        this.onError('Error ao buscar os cursos.');
+        return of([]);
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
+  }
+}
